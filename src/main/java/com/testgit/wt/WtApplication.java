07e1997e1ff25651e0dc6788d7014a474665d7b8
wtnn <1296148157@qq.com>
2019-08-19 15:51:47 +0800

    wt第一次上传

---
+++ src/main/java/com/testgit/wt/WtApplication.java
@@ -0,0 +1,13 @@
+package com.testgit.wt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WtApplication.class, args);
+    }
+
+}
